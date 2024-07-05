1b1b2e79393af7347a7893d9a5e7757e4cebc4cf
frank <30297768+ilknarf@users.noreply.github.com>
2024-07-05 18:23:33 -0500

    add org roam

--- init.el
+++ init.el
@@ -132,6 +132,24 @@
   :after org
   :hook (org-mode . org-superstar-mode))
 
+;;; org roam
+(use-package org-roam :ensure t
+  :after org
+  :custom
+  (org-roam-directory (file-truename "~/zk"))
+  ;; copied from https://github.com/org-roam/org-roam
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode))
+
 ;;; seq (required update for transient)
 (use-package seq :ensure t)
 
